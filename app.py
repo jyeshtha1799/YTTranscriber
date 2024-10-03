7680a92eb9f11825a36a5121826615c08f1ae7d6
jyeshtha1799 <114448454+jyeshtha1799@users.noreply.github.com>
2024-10-03 00:39:32 -0400

    youtube transcriber pai settings done plus app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound, TranscriptsDisabled
+
+# Load environment variables
+load_dotenv()
+
+# Configure Google Generative AI
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+prompt = """You are a YouTube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
+
